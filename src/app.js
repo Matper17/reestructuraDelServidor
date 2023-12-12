a33831112b53de9c5d11021f2eba4e9cdc3f8979
Matper17 <matiasperalta992@gmail.com>
2023-12-11 22:55:47 -0300

    Desafio reestructurando servidor

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import usersRouter from "./routes/users.router.js";
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", usersRouter);
+
+//const PORT = 8080;
+
+app.listen(8080, () => {
+  console.log("Escuchando al puerto 8080");
+});
